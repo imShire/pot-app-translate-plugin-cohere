093ba20381b51afaeaaa1f959ee984ebdd63831b
imShire <im@ixu.me>
2024-04-20 08:29:17 +0800

    feat: add pot-app-translate-plugin-cohere

---
+++ README.md
@@ -0,0 +1,55 @@
+# Pot-App Cohere AI 翻译插件
+
+## 特征
+- 支持 Command R+, Command R, Command, Command Nightly, Command Light, Command Light Nightly .
+- 暂不支持流响应。
+- 支持翻译、润色、提问、自定义提示。
+
+## 支持平台
+
+- [x] Windows
+  - [x] x64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/x86_64-pc-windows-msvc.zip)
+  - [x] x86 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/i686-pc-windows-msvc.zip)
+  - [x] aarch64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/aarch64-pc-windows-msvc.zip)
+- [x] Linux
+  - [x] x64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/x86_64-unknown-linux-gnu.zip)
+  - [x] x86 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/i686-unknown-linux-gnu.zip)
+  - [x] aarch64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/aarch64-unknown-linux-gnu.zip)
+  - [x] armv7 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/armv7-unknown-linux-gnueabihf.zip)
+- [x] MacOS
+  - [x] x64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/x86_64-apple-darwin.zip)
+  - [x] aarch64 [下载](https://gh.pylogmon.com/https://github.com/imShire/pot-app-translate-plugin-cohere/releases/latest/download/aarch64-apple-darwin.zip)
+
+## 使用方法
+
+1. 下载对应平台的插件，解压得到 `.potext` 文件
+2. 打开Pot-偏好设置-服务设置-翻译-添加外部插件-安装外部插件
+3. 选择刚刚解压得到的 `.potext` 文件，安装成功
+4. 将插件添加到服务列表即可使用
+
+## 设置代理域名
+```sh
+# cohere.json
+{
+  "version": 2,
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "https://api.cohere.ai/$1",
+      "headers": {
+        "Cache-Control": "no-cache",
+        "Host": "api.cohere.ai",
+        "origin": "https://api.cohere.ai",
+        "referer": "https://api.cohere.ai/",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+      }
+    }
+  ]
+}
+```
+执行 `vercel -A cohere.json --prod` 并绑定域名部署代理
+
+
+## 作者
+pot-app-translate-plugin-cohere © [imShire](https://github.com/imShire), Released under the [GPL 3.0](https://github.com/imShire/pot-app-translate-plugin-cohere/blob/main/LICENSE) License.
+
---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::error::Error;
+
+#[no_mangle]
+pub fn translate(
+    text: &str, // 待翻译文本
+    from: &str, // 源语言
+    to: &str,   // 目标语言
+    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
+    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
+    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+) -> Result<Value, Box<dyn Error>> {
+    let client = reqwest::blocking::ClientBuilder::new().build()?;
+    let api_url = match needs.get("apiUrl") {
+        Some(raw_url) => {
+            if !raw_url.starts_with("http") {
+                format!("https://{raw_url}")
+            } else {
+                raw_url.to_string()
+            }
+        }
+        None => {
+            String::from("https://api.cohere.ai")
+        }
+    };
+
+    let model = match needs.get("model") {
+        Some(raw_model) => {
+            raw_model.to_string()
+        }
+        None => {
+            String::from("command-r-plus")
+        }
+    };
+
+    let mode = match needs.get("model") {
+        Some(raw_mode) => {
+            raw_mode.to_string()
+        }
+        None => {
+            String::from("1")
+        }
+    };
+    let customize_prompt = match needs.get("customizePrompt") {
+        Some(raw_prompt) => {
+            raw_prompt.to_string()
+        }
+        None => {
+            String::from("")
+        }
+    };
+    let apikey = needs.get("apiKey");
+    // let  api_url = needs.get("apiUrl");
+    let api_url_path = "/v1/chat";
+    if apikey.unwrap_or(&&"".to_string()).is_empty() {
+        return Err("apiKey is required".into());
+    }
+    println!("using default: \n{}\n{}\n{}\n{}\n", api_url,model,mode,apikey.unwrap());
+    let full_url = format!("{}{}", api_url, api_url_path);
+    let auth_header = format!("bearer {}", apikey.unwrap());
+    let body = build_request_body(&model, &mode, &customize_prompt, text, from, to);
+    println!("body: \n{}\n{}\n{}\n", full_url,auth_header,body);
+    let res = client
+        .post(&full_url)
+        // .header("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.32(0x18002035) NetType/WIFI Language/zh_TW")
+        .header("Content-Type", "application/json")
+        .header("accept", "application/json")
+        .header("Authorization", &auth_header)
+        .json(&body)
+        .send()?
+        .json()?;
+
+    println!("res: \n{}", res);
+    fn parse_result(res: Value) -> Option<String> {
+        let result = res
+        .get("text")?
+        .as_str()?
+        .to_string();
+        println!("res: \n{}",result);
+        Some(result)
+    }
+    if let Some(result) = parse_result(res) {
+        return Ok(Value::String(result));
+    } else {
+        return Err("Response Parse Error".into());
+    }
+}
+
+fn build_request_body(model: &str, mode: &str, customize_prompt: &str, text: &str, from: &str, to: &str) -> Value {
+    let prompt = generate_prompts(mode, customize_prompt,from,to);
+    json!({
+        "model": model,
+        "chat_history": [{"role": "SYSTEM", "message": prompt}],
+        "message": text,
+        "stream": false,
+        "maxTokens": 4000
+    })
+}
+
+fn generate_prompts(mode: &str, customize_prompt: &str, from: &str, to: &str) -> String {
+    let translation_prompt = "You are a professional translation engine, please translate the text into a colloquial, professional, elegant and fluent content, without the style of machine translation. You must only translate the text content, never interpret it.";
+    let user_prompt = match mode {
+        "1" => {
+            format!("{} from {} to {}.", translation_prompt, from, to)
+        },
+        "2" => {
+            format!("{} Embellish in  {} : {}.", "You are a text embellisher, you can only embellish the text, never interpret it.", from, to)
+        },
+        "3" => {
+            format!("{} Embellish in  {} : {}.", "Please answer the following question", from, to)
+        },
+        _ => {
+            format!("{}", customize_prompt)
+        }
+    };
+    user_prompt
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn try_request() {
+        let needs = HashMap::new();
+        let result = translate("Hello", "auto", "zh", "en", needs).unwrap();
+        println!("{result:?}");
+    }
+}
